ad7adbf2433c029a30e8de6cf48a09966d251975
Wei Bin Wong <wongwb1990@gmail.com>
2022-07-30 17:21:31 +0800

    project setup installed node.js, mongoose, ejs, express, express-session and dotenv

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+
+const app = express()
+const port = process.env.PORT || 3000
+const connStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@generalassembly.cfpcvlq.mongodb.net/?retryWrites=true&w=majority`
+
+// Set view engine
+app.set('view engine', 'ejs')
+
+// Apply session middlewares
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: false} // cookie setting
+  }))
+
+// routes
+app.get('/', (req, res) => {
+    res.send('ok')
+})
+
+app.listen(port, async () => {
+  try {
+      await mongoose.connect(connStr, { dbName: 'Galaxy_Atelier'})
+  } catch(err) {
+      console.log(`Failed to connect to DB`)
+      process.exit(1)
+  }
+
+  console.log(`Example app listening on port ${port}`)
+})
